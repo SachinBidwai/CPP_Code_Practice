815dd88f3b1433f35a9dfae0efae5f9697df148f
Sachin Bidwai <sachinbidwai1982@gmail.com>
2025-03-06 22:43:10 +0530

    List
    List

--- CPP_STL/list/main.cpp
+++ CPP_STL/list/main.cpp
@@ -1,19 +1,63 @@
-// List 
+// STL : List
 
 #include<iostream>
 #include<string>
 #include<list>
 using namespace std;
 
+
 int main()
 {
-	list<int> age = { 23, 12, 36, 27, 18, 19 };
-
-	for (int i : age)
+	list<string> cars = { "Volvo", "BMW", "Ford", "Mazda" };
+	cout << "{ ";
+	for (string car : cars)
 	{
-		cout << i << ", ";
+		cout << car << ", ";
 	}
+	cout << " }";
 	cout << "\n\n";
+
+	cout << "Car List front element : " << cars.front()<< endl;
+	cout << "Car List Back element : " << cars.back()<< endl;
+
+	cout << "Change a List Element : " << endl;
+	cars.front() = "Honda";
+	cars.back() = "i20";
+
+	cout << endl << "{ ";
+	for (string car : cars)
+	{
+		cout << car << ", ";
+	}
+	cout << " }";
+	cout << "\n\n";
+
+	cout << "Add List Elements : " << endl;
+	cars.push_front("Tesla");
+	cars.push_back("Suzuki");
+
+	cout << endl << "{ ";
+	for (string car : cars)
+	{
+		cout << car << ", ";
+	}
+	cout << " }";
+	cout << "\n\n";
+
+
+	cout << "Remove List Elements : " << endl;
+	cars.pop_front();
+	cars.pop_back();
+
+	cout << endl << "{ ";
+	for (string car : cars)
+	{
+		cout << car << ", ";
+	}
+	cout << " }";
+	cout << "\n\n";
+
+	// cout << "List Size : " << endl;
 
 
 
