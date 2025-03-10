c41c4376ea108015ed728c813f67ee23d8e6e918
Sachin Bidwai <sachinbidwai1982@gmail.com>
2025-03-10 12:19:57 +0530

    Iterator Updates 1
    Iterator Updates 1

--- CPP_STL/Iterator/main.cpp
+++ CPP_STL/Iterator/main.cpp
@@ -13,42 +13,46 @@ using namespace std;
 
 int main()
 {
-	// Create Container : 
-	
-	// Create a vector called cars that will store strings
-	vector<string> cars = { "Volvo", "BMW", "Ford", "Mazda" };
+    // Create Containers:
+    vector<string> cars = { "Volvo", "BMW", "Ford", "Mazda" };
+    list<string> names = { "Sachin", "Kaustubh", "Dipali", "Ruthvij" };
+    list<int> numbers = { 28, 45, 23, 38, 87, 49, 56, 98 };
 
-	// Create a vector called cars that will store strings
-	list<string> names = { "Sachin", "Kaustubh", "Dipali", "Ruthvij" };
+    // Display Vector Cars:
+    cout << "\nNames from Vector Cars: ";
+    cout << "{ ";
+    bool first = true;  // Reset before loop
+    for (auto it = cars.begin(); it != cars.end(); ++it)
+    {
+        if (!first) cout << ", ";
+        cout << *it;
+        first = false;
+    }
+    cout << " }\n";
 
+    // Display List Names:
+    cout << "\nNames from List Names: ";
+    cout << "{ ";
+    first = true;  // Reset before loop
+    for (auto it_1 = names.begin(); it_1 != names.end(); ++it_1)
+    {
+        if (!first) cout << ", ";
+        cout << *it_1;
+        first = false;
+    }
+    cout << " }\n";
 
+    // Display List Numbers:
+    cout << "\nNumbers from List Numbers: ";
+    cout << "{ ";
+    first = true;  // Reset before loop
+    for (auto it_2 = numbers.begin(); it_2 != numbers.end(); ++it_2)
+    {
+        if (!first) cout << ", ";
+        cout << *it_2;
+        first = false;
+    }
+    cout << " }\n";
 
-	// Create iterator: 
-	
-	// Create a vector iterator called it
-	vector<string> ::iterator it;
-
-	// Create a list iterator called it_1
-	list<string> ::iterator it_1;
-
-
-	// Display Container : 
-	
-	// Loop through the vector with the iterator
-	cout << "\nNames from Vector Cars : " << endl;
-	for (it = cars.begin(); it != cars.end(); ++it)
-	{
-		cout << *it << endl;
-	}
-
-
-	// Loop through the vector with the iterator
-	cout << "\nNames from list names : " << endl;
-	for (it_1 = names.begin(); it_1 != names.end(); ++it_1)
-	{
-		cout << *it_1 << endl;
-	}
-
-
-	return 0;
+    return 0;
 }
