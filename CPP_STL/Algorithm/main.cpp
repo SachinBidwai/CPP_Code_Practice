09d2aeffd05a112c789c7047a5d59a727f47d72f
Sachin Bidwai <sachinbidwai1982@gmail.com>
2025-03-11 21:47:25 +0530

    Sort Vector by Algorithm
    Sort Vector by Algorithm

--- CPP_STL/Algorithm/main.cpp
+++ CPP_STL/Algorithm/main.cpp
@@ -9,11 +9,12 @@ using namespace std;
 int main()
 {
 	vector<string> cars = { "Volvo", "BMW", "Ford", "Mazda" };
+	vector<int> numbers = { 24, 13, 14, 11, 19, 16, 23, 9, 5, 33 };
 
 	// Sort cars names in alphabetically Ascending order:
 	sort(cars.begin(), cars.end());
 
-	// Display cars 
+	// Display Cars names arrange in alphabetically Ascending order 
 	cout << "Sorting algorithm : Cars names arrange in alphabetically Ascending order: { ";
 	bool first = true;
 
@@ -28,7 +29,7 @@ int main()
 	// Sort cars names in alphabetically Descending order:
 	sort(cars.rbegin(), cars.rend());
 
-	// Display cars 
+	// Display Cars names arrange in alphabetically Descending order
 	cout << "Sorting algorithm : Cars names arrange in alphabetically Descending order: { ";
 	first = true;
 
@@ -36,6 +37,34 @@ int main()
 	{
 		if (!first) cout << ", ";
 		cout << car;
+		first = false;
+	}
+	cout << " }\n\n";
+
+	cout << "Initial Vector Number is  : { ";
+
+	first = true;
+
+	for (int num : numbers)
+	{
+		if (!first) cout << ", ";
+		cout << num;
+		first = false;
+	}
+	cout << " }\n\n";
+
+	// Sort numbers numerically, starting from the fourth element
+	sort(numbers.begin() + 3, numbers.end());
+
+
+	cout << "Sort numbers numerically, starting from the fourth element : { ";
+
+	first = true;
+
+	for (int num : numbers)
+	{
+		if (!first) cout << ", ";
+		cout << num;
 		first = false;
 	}
 	cout << " }\n\n";
