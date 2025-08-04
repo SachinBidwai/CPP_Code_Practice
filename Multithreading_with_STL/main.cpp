0111d2bf2991d8fdd64909b41d4f71bf9b362901
Sachin Bidwai <sachinbidwai1982@gmail.com>
2025-08-04 22:00:38 +0530

    Multithreading with STL
    Multithreading with STL

---
+++ Multithreading_with_STL/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <thread>
+using namespace std;
+
+void printSquare(int n) {
+    cout << "Square of " << n << " is " << n * n << endl;
+}
+
+int main() {
+    thread t1(printSquare, 4);
+    thread t2(printSquare, 5);
+    t1.join();
+    t2.join();
+    return 0;
+}
